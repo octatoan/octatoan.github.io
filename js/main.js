a53762288fc92fde250d08fb066175ca2bb0803c
Soham Chowdhury <octatoan@gmail.com>
2015-06-22 19:19:23 +0530

    MathJax

--- js/main.js
+++ js/main.js
@@ -1,22 +1,7 @@
 var elements = document.getElementsByTagName('script')
-
-Array.prototype.forEach.call(elements, function(element) {
-    if (element.type.indexOf('math/tex') != -1) {
-        // Extract math markdown
-        var textToRender = element.innerText || element.textContent;
-
-        // Create span for KaTeX
-        var katexElement = document.createElement('span');
-
-        // Support inline and display math
-        if (element.type.indexOf('mode=display') != -1){
-            katexElement.className += "math-display";
-            textToRender = '\\displaystyle {' + textToRender + '}';
-            } else {
-                katexElement.className += "math-inline";
-            }
-
-            katex.render(textToRender, katexElement);
-            element.parentNode.insertBefore(katexElement, element);
-        }
-    });
+<script type="text/x-mathjax-config">
+  MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+  </script>
+  <script type="text/javascript"
+    src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+    </script>
